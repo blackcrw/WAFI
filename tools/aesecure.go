a66832d15524020283dc4b7e86bb221f168b2253
B L Λ C W <37225059+blackcrw@users.noreply.github.com>
2021-10-13 15:13:39 -0300

    ✨ Base ✨ #card-70512044
    https://github.com/blackcrw/akumascan/projects/1#card-70512044

--- tools/aesecure.go
+++ tools/aesecure.go
@@ -1,9 +1,13 @@
+/* https://github.com/blackcrw/akumascan/projects/1#card-70649811 */
+
 package tools
 
-import "fmt"
+import (
+	"github.com/blackcrw/akumascan/pkg/nettools"
+)
 
-func aesecure(content_raw string) bool {
-	fmt.Println("AESECURE - TEST")
+func aesecure(channel chan [3]string, quit chan bool, response *nettools.Response) {
+	channel<-[3]string{"AESecure", "", "teste"}
 
-	return true
+	quit<-true
 }
